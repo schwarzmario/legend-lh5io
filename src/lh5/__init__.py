3182671536821d640b3df9a2abccb23e9f6be639
George Marshall <george.marshall.20@ucl.ac.uk>
2026-01-12 20:52:32 -0800

    add version

--- src/lh5/__init__.py
+++ src/lh5/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from ._version import version as __version__
 
 # import this so users can transparently decode data compressed with hdf5plugin
 # filters
@@ -7,6 +8,7 @@ import hdf5plugin  # noqa: F401
 from .io import LH5Iterator, LH5Store, ls, read, read_as, read_n_rows, show, write
 
 __all__ = [
+    "__version__",
     "LH5Iterator",
     "LH5Store",
     "ls",
